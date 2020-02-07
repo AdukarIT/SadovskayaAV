c6811874d048238011ebcdad82e53ce9879125c5
sasha <sashasadovskaya@gmail.com>
2020-02-07 23:30:57 +0300

    add 4 task and fixed 2,3 tasks

--- js/array/script.js
+++ js/array/script.js
@@ -1,62 +1,78 @@
 
 //task 1 
-let array = [];
-function getRandomArray(len){
-		for( let i = 0; i < len; i++){
-			array [i] =  Math.floor(Math.random()*10);
-		}
-		console.log(array);
+console.group('task1: Generate random array')
+
+
+function getRandomArray(len = 10){
+	let array = [];
+	for( let i = 0; i < len; i++){
+		array [i] =  Math.floor(Math.random()*10);
+	}
+	console.log("Random array: " , array);
+	return array;
 }
-console.log(getRandomArray(10));
+getRandomArray();
+console.groupEnd();
   
 
-//task 2
-
-function getNumber(){
+// task 2
+console.group('task2: Write digits greater then average')
+function getNumber(array){
 	let sum = 0;
 	for(let i = 0; i < array.length; i++){
 		sum = sum + (array[i]);
 	}
 	let mid = sum / array.length;
+	console.log("average", mid)
+	let gta = [];
 	for(let i = 0; i < array.length; i++){
 		if (array[i] > mid) {
-			console.log(array[i])
+			gta.push(array[i]);
 		}
 	}
+	return gta;
 }
-console.log(getNumber());
+console.log('Result: ', getNumber(getRandomArray()));
+console.groupEnd();
 
 
-//task 3
 
-// function getSmallest(){
-// 	let s = array[0];
-// 	for(let i = 1;i< array.length; i++ ){
-// 		if (array[i] < s) {	
-// 			s = array[i]
-// 		}
-// 	}
-// 	console.log(s)
-
-// }
-// console.log(getSmallest());
-
-function getSmallest(){
-	let small = '';
+// //task 3
+console.group('task3: Write last two smallest digits')
+function getSmallest(array){
    	for (let i = 0; i< array.length; i++){
-    	for (let j=0; j< array.length; j++){
+   	 	for (let j=0; j< array.length; j++){
         	if (array[i] > array[j]){
-	            let bols = array[i];
-	            array [i] = array [j];
-	            array [j]= bols;
+		        let bols = array[i];
+		        array [i] = array [j];
+		        array [j]= bols;
         	} 
     	}
    	}   
-  	console.log(array);
-		small = array.pop() + " " + array.pop();
 	
-  	console.log(small);
+	return [array.pop(), array.pop()];
 }
-console.log(getSmallest());
+console.log("Result: ", getSmallest(getRandomArray()));
+console.groupEnd();
 
+
+//task 4
+console.group('task4: Remove digits less then 3')
+function replaceZeros(array){
+	let n = 0;
+	for(let i = 0; i < array.length; i++){
+		if ((array.length - i) === n ) break;
+		if (array[i] < 3) {
+			n++;
+			for(let j = i; j < array.length - 1; j++){
+				array[j] = array[j + 1];
+			}
+			array[array.length - 1] = 0;
+			i--;
+		}
+	}
+	return array;
+}
+console.log("Result: ", replaceZeros(getRandomArray()))
+console.groupEnd();
 
