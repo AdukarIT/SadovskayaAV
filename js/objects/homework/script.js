8e606c6d81d005f6234c45897b0803f1e7e0b3ee
asadovskaya <sashasadovskaya@gmail.com>
2020-02-11 18:43:09 +0300

    add task 3, 4

--- js/objects/homework/script.js
+++ js/objects/homework/script.js
@@ -25,7 +25,42 @@ let recipe = {
 function getRecipe(){
     for (let prop in recipe );
     for (let propIng in recipe.ingredients )
-        console.log(propIng + " " +  recipe.ingredients[propIng]/recipe.portions +  'gram')
+        console.log(propIng + " " +  recipe.ingredients[propIng]/recipe.portions +  ' gram')
 }
 getRecipe();
+console.groupEnd();
+
+
+//3
+console.group("task3: calculate cylinder volume")
+let cylinder = {
+    r: 5,
+    h: 6,
+}
+cylinder.v = function(){
+    let volume = 3.14 * this.r**2 * this.h;
+    return volume;
+}
+console.log(cylinder.v())
+console.groupEnd();
+
+// 4
+console.group('task4: Сreate an object and add methods to it ')
+let penguin = {
+    name: "Captain Cook",
+    author: "Richard and Florence Atwater",
+    source: "Mr. Popper's Penguins",
+}
+penguin.canFly = false;
+penguin.sayHi = function(){
+    console.log("Hi! My name is " + penguin.name + ". You can meet me in the book " + penguin.source + " which wrote " + penguin.author)
+}
+penguin.sayHi();
+penguin.fly = function(){
+    if (this.canFly){
+        console.log(("I can fly"))
+    }
+    console.log("I can not fly")
+}
+console.log(penguin.fly());
 console.groupEnd();
