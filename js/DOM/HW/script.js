c4648c477eed3452e581077eca34b29538721e31
asadovskaya <sashasadovskaya@gmail.com>
2020-02-26 23:59:04 +0300

    add directory

---
+++ js/DOM/HW/script.js
@@ -0,0 +1,12 @@
+//1
+let div = document.getElementById("elem");
+div.classList.add("www");
+console.log(div);
+
+//2
+div.classList.contains("www");
+if (div.classList.contains("www")){
+    div.classList.remove("www")
+}
+console.log(div);
+
