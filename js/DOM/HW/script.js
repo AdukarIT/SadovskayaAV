0c28795374e4739f5e70981637fd74e193341901
asadovskaya <sashasadovskaya@gmail.com>
2020-02-27 22:06:41 +0300

    add task 4

--- js/DOM/HW/script.js
+++ js/DOM/HW/script.js
@@ -10,3 +10,14 @@ if (div.classList.contains("www")){
 }
 console.log(div);
 
+//4
+function createUl() {
+    let  array = [ 1, 2 , 3, 4, 5, 6];
+    for (let i = 0; i < array.length; i ++) {
+        let li = document.createElement('li');
+        li.innerHTML = array[i];
+        ul.append(li);
+    }
+}
+console.log(createUl());
+
