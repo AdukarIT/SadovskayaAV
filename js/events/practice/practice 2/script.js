f6729b6a8eb827fa67baa02e1f8b9bc9efa56f86
asadovskaya <sashasadovskaya@gmail.com>
2020-03-01 14:41:15 +0300

    add task 3

--- js/events/practice/practice 2/script.js
+++ js/events/practice/practice 2/script.js
@@ -9,3 +9,18 @@ const folder = document.getElementById("folder");
 folder.ondblclick = function (evt) {
   this.style.backgroundImage = "url('img/opened.png')"
 };
+
+//3
+const container = document.getElementById("container");
+for (let i = 1; i <= 400; i++){
+    let div = document.createElement("div");
+    div.style.width = "30px";
+    div.style.height = "30px";
+    div.style.backgroundColor = "red";
+    div.style.margin = "2px";
+    div.style.transition = "1s";
+    div.onmouseover = function (evt) {
+        this.style.borderRadius = "15px"
+    };
+    container.append(div);
+}
