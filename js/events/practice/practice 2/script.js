877003a07716335ed3c880262fe6cf0438ed1e11
asadovskaya <sashasadovskaya@gmail.com>
2020-03-01 14:16:58 +0300

    add task 2

--- js/events/practice/practice 2/script.js
+++ js/events/practice/practice 2/script.js
@@ -1,4 +1,11 @@
 //1
-block.onclick = function showCoords(evt) {
+const block = document.getElementById("block");
+block.onmousemove = function showCoords(evt) {
     document.getElementById('coords').innerHTML = "x = " + evt.clientX  + ", y = " + evt.clientY;
+};
+
+//2
+const folder = document.getElementById("folder");
+folder.ondblclick = function (evt) {
+  this.style.backgroundImage = "url('img/opened.png')"
 };
