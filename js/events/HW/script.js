4c1aeeec77e6a5819ac472904f1e5eb12a4351ae
asadovskaya <sashasadovskaya@gmail.com>
2020-03-03 13:56:04 +0300

    add hw task 2

--- js/events/HW/script.js
+++ js/events/HW/script.js
@@ -5,3 +5,25 @@ button.addEventListener('click', function () {
     let clone = input.cloneNode(true);
     input.parentElement.appendChild(clone)
 });
+
+//2
+let table = document.getElementById("table");
+let counter = 0;
+table.onclick = function () {
+    switch (counter) {
+        case 0:
+            table.classList.remove("highlight")
+            table.classList.add("bordered");
+            break;
+        case 1:
+            table.classList.remove("bordered");
+            table.classList.add("striped");
+            break;
+        case 2:
+            table.classList.remove("striped");
+            table.classList.add("highlight");
+            counter = 0;
+            return;
+    };
+    counter++;
+};
