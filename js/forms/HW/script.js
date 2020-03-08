fd36df2a50352a2ecf2c09f9cb6ae08f62af034f
asadovskaya <sashasadovskaya@gmail.com>
2020-03-08 22:26:46 +0300

    add task 3

--- js/forms/HW/script.js
+++ js/forms/HW/script.js
@@ -4,9 +4,9 @@ let name = form.elements.name;
 let time = form.elements.minutes;
 let submit = form.elements.submit;
 let message = document.getElementById("message");
-let message2 = document.getElementById("message2")
+let message2 = document.getElementById("message2");
 form.onsubmit = function validate() {
-    if(name.value.length == 0) {
+    if (name.value.length == 0) {
         message.innerHTML = "*данное поле обязательно для заполнения";
         return false;
     }
@@ -14,4 +14,35 @@ form.onsubmit = function validate() {
         message2.innerHTML = "*Введите время В ЦИРФАХ";
         return false;
     }
- };
+};
+//3
+form2 = document.forms[1];
+let fields = form2.getElementsByClassName("field");
+form2.onsubmit = function validate2() {
+    for (let i = 0; i < fields.length; i++) {
+        let input = fields[i];
+        switch (input.type) {
+            case "email":
+                if (!(/^.{0,100}@[\w\-]{2,20}\.[a-z]{2,15}$/i.test(input.value))) {
+                    console.log("wrong email")
+                    return false;
+                }
+                break;
+            case "number":
+                if (!(/^\d+$/.test(input.value))) {
+                console.log("wrong number");
+                return false;
+                }
+                break;
+            case "text":
+            default:
+                if (!input.value) {
+                    console.log('поле не заполнено', input);
+                    return false;
+                }
+        }
+    }
+    return false;
+};
+
+
