1202242de8edfcc3957820ad3576d2e36ee2f744
asadovskaya <sashasadovskaya@gmail.com>
2020-03-09 10:48:07 +0300

    add GGS

---
+++ js/project/script.js
@@ -0,0 +1,26 @@
+let db = [{
+    name: 'Габриэль Гарсиа Маркес',
+    birthday: '06-03-1927',
+    deathday: '17-04-2014',
+    country: 'Колумбия',
+    photo: 'https://upload.wikimedia.org/wikipedia/commons/0/0f/Gabriel_Garcia_Marquez.jpg',
+    books: [
+        {
+            name: 'Сто лет одиночества',
+            published: 1967,
+            cover: 'https://upload.wikimedia.org/wikipedia/ru/9/9d/100years.jpg',
+            genre: 'роман',
+        },
+        {
+            name: 'Любовь во время чумы',
+            published: 1985,
+            cover: 'https://i.livelib.ru/boocover/1002072601/200/6cf4/Gabriel_Garsia_Markes__Lyubov_vo_vremya_chumy.jpg',
+            genre: ['роман']
+        },
+        {
+            name: 'Полковнику никто не пишет',
+            published: 1961,
+            cover: 'https://www.livelib.ru/book/1000318322-polkovniku-nikto-ne-pishet-gabriel-garsia-markes',
+            genre: 'повесть',
+        },
+    ];
